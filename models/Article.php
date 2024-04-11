fa9803cb522166a952ac1d2e74807fb25ca1def3
johlbaum <julien.ohlbaum@gmail.com>
2024-04-12 00:09:42 +0200

    Fix bug by updating parameter types in setDateUpdate function

--- models/Article.php
+++ models/Article.php
@@ -110,7 +110,7 @@ class Article extends AbstractEntity
      * @param string $format : le format pour la convertion de la date si elle est une string.
      * Par défaut, c'est le format de date mysql qui est utilisé.
      */
-    public function setDateUpdate(string|DateTime $dateUpdate, string $format = 'Y-m-d H:i:s'): void
+    public function setDateUpdate(string|DateTime|null $dateUpdate, string $format = 'Y-m-d H:i:s'): void
     {
         if (is_string($dateUpdate)) {
             $dateUpdate = DateTime::createFromFormat($format, $dateUpdate);
