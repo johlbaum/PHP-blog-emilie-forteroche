853c014cc5e05cdde123a9af4b87c963b09e315e
johlbaum <julien.ohlbaum@gmail.com>
2024-05-02 21:44:01 +0200

    Add return type declaration to the showApropos function

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -64,7 +64,7 @@ class ArticleController
      * Affiche la page "à propos".
      * @return void
      */
-    public function showApropos()
+    public function showApropos(): void
     {
         $view = new View("A propos");
         $view->render("apropos");
